35425930e29902011665eed0be986d1d9c822d09
Lingtong Meng <149747743+mlt0727@users.noreply.github.com>
2026-04-14 02:32:20 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+        connect = set()
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != "O" or (r, c) in connect:
+                return
+            connect.add((r, c))
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for c in range(cols):
+            if board[0][c] == "O":
+                dfs(0, c)
+            if board[rows - 1][c] == "O":
+                dfs(rows - 1, c)
+        for r in range(rows):
+            if board[r][0] == "O":
+                dfs(r, 0)
+            if board[r][cols - 1] == "O":
+                dfs(r, cols - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O" and (r,c) not in connect:
+                    board[r][c] = "X"
